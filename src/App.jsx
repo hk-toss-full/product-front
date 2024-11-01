32ce888b0e0fce3212bf7bd88cb2ed08125c2610
Chaemin-L <chaem12@kookmin.ac.kr>
2024-11-01 13:59:09 +0900

    fix: baseurl 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-import axios from 'axios'
-import { useEffect } from 'react'
-
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
 /*
 product 
 id name quantity price
@@ -11,92 +10,102 @@ sell quantity - 1
 receiving goods + 1
 */
 const initProduct = {
-  name:"",
-  price:0,
-}
-const getBaseURL = ()=>{
-  return import.meta.env.PROD ? `http://${location.hostname}` : "http://localhost:8080";
-}
+  name: "",
+  price: 0,
+};
+const getBaseURL = () => {
+  return import.meta.env.PROD
+    ? `http://${location.hostname}`
+    : "http://localhost:8080";
+};
 
+axios.defaults.baseURL = import.meta.env.PROD || "http://localhost:8080";
 
-const getAllProductsApi = async () =>{
+const getAllProductsApi = async () => {
   const { data } = await axios({
-    method:"GET",
-    url:`${getBaseURL()}/api/v1/products`
-  })
-  return data
-}
+    method: "GET",
+    url: `${getBaseURL()}/api/v1/products`,
+  });
+  return data;
+};
 const sellingApi = async (id) => {
-  const {data} = await axios({
-    method: "PUT", url:`${getBaseURL()}/api/v1/products/${id}/selling`
-  })
-}
+  const { data } = await axios({
+    method: "PUT",
+    url: `${getBaseURL()}/api/v1/products/${id}/selling`,
+  });
+};
 const receivingApi = async (id) => {
-  const {data} = await axios({
-    method: "PUT", url:`${getBaseURL()}/api/v1/products/${id}/receiving`
-  })
-}
-const addProductApi = async (product) =>{
-  const {data} = await axios({
-    method: "POST", url:`${getBaseURL()}/api/v1/products`,
-    data: product
-  })
-}
+  const { data } = await axios({
+    method: "PUT",
+    url: `${getBaseURL()}/api/v1/products/${id}/receiving`,
+  });
+};
+const addProductApi = async (product) => {
+  const { data } = await axios({
+    method: "POST",
+    url: `${getBaseURL()}/api/v1/products`,
+    data: product,
+  });
+};
 
 function App() {
-  console.log(import.meta.env)
-  const [product, setProduct] = useState({...initProduct })
-  const [products, setProducts] = useState([])
-  const getAllProducts = async()=>{
-    const data = await getAllProductsApi()
-    setProducts([...data])
-  }
-  useEffect(()=>{
-    getAllProducts()
-  },[])
-  const onChangeHandler =(e)=>{
-    const {name, value} = e.target;
-    setProduct({...product,[name]:value});
-  }
-  const addProduct = (e)=>{
-    e.preventDefault()
+  const [product, setProduct] = useState({ ...initProduct });
+  const [products, setProducts] = useState([]);
+  const getAllProducts = async () => {
+    const data = await getAllProductsApi();
+    setProducts([...data]);
+  };
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
+  const addProduct = (e) => {
+    e.preventDefault();
     addProductApi(product);
-    setProduct({...initProduct})
+    setProduct({ ...initProduct });
     // setProducts([...products, {...product, quantity:0, id: products.length+1}])
-    
-  }
-  const addQuantity =(id)=>{
-    receivingApi(id)
+  };
+  const addQuantity = (id) => {
+    receivingApi(id);
     // const newProducts = products.map((el)=>el.id === id ? {...el, quantity: el.quantity+1}:el)
     // setProducts(newProducts)
-  }
-  const sellQuantity =(id)=>{
-    const product = products.find(el=> el.id === id && el.quantity > 0)
-    if(!product) {
-      alert("재고 부족")
-      return
+  };
+  const sellQuantity = (id) => {
+    const product = products.find((el) => el.id === id && el.quantity > 0);
+    if (!product) {
+      alert("재고 부족");
+      return;
     }
     sellingApi(id);
     // const newProducts = products.map((el)=>el.id === id ? {...el, quantity: el.quantity-1}:el)
     // setProducts(newProducts)
-  }
+  };
 
   return (
-    <div style={{display:'flex', gap:30}}>
+    <div style={{ display: "flex", gap: 30 }}>
       <div>
         <h1>제품 등록</h1>
-        <hr/>
+        <hr />
         <form onSubmit={addProduct}>
-          <input name='name' onChange={onChangeHandler} value={product.name}/><br/>
-          <input name='price' onChange={onChangeHandler} value={product.price}/><br/>
-          <button >등록</button>
+          <input name="name" onChange={onChangeHandler} value={product.name} />
+          <br />
+          <input
+            name="price"
+            onChange={onChangeHandler}
+            value={product.price}
+          />
+          <br />
+          <button>등록</button>
         </form>
       </div>
       <div>
         <h1>제품 보기</h1>
-        <hr/>
+        <hr />
         <button onClick={getAllProducts}>새로고침</button>
-        <hr/>
+        <hr />
         <table>
           <thead>
             <th>id</th>
@@ -107,21 +116,25 @@ function App() {
             <th></th>
           </thead>
           <tbody>
-            {products.map((el)=>
+            {products.map((el) => (
               <tr key={el.id}>
                 <td>{el.id}</td>
                 <td>{el.name}</td>
                 <td>{el.quantity}</td>
                 <td>{el.price}</td>
-                <td><button onClick={()=>sellQuantity(el.id)}>사기</button></td>
-                <td><button onClick={()=>addQuantity(el.id)}>채우기</button></td>
-              </tr>  
-            )}
+                <td>
+                  <button onClick={() => sellQuantity(el.id)}>사기</button>
+                </td>
+                <td>
+                  <button onClick={() => addQuantity(el.id)}>채우기</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
